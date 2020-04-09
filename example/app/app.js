aa7b278cf7180d9b8bffb205246869c1708456b2
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-04-09 11:34:14 +0200

    feat(example): save postit board as xml and svg

--- example/app/app.js
+++ example/app/app.js
@@ -12,39 +12,84 @@ import diagramXML from '../resources/newDiagram.postit';
 var modeler = new PostItModeler({
   container: '#canvas',
   keyboard: {
-    bindTo: window
-  }
+    bindTo: window,
+  },
 });
 
-
-function exportDiagram() {
-
-  modeler.saveXML({ format: true }, function(err, xml) {
-
-    if (err) {
-      return console.error('could not save postit diagram', err);
-    }
-
-    alert('Diagram exported. Check the developer tools!');
-
-    console.log('DIAGRAM', xml);
-  });
-}
-
-
 function openDiagram(bpmnXML) {
-
   // import diagram
   modeler.importXML(bpmnXML, function(err) {
-
     if (err) {
       return console.error('could not import postit diagram', err);
     }
   });
 }
 
+function saveSVG(done) {
+  modeler.saveSVG(done);
+}
+
+function saveDiagram(done) {
+
+  modeler.saveXML({ format: true }, function(err, xml) {
+    done(err, xml);
+  });
+}
+
+// bootstrap diagram functions
+$(function() {
+
+  var downloadLink = $('#js-download-diagram');
+  var downloadSvgLink = $('#js-download-svg');
+
+  $('.buttons a').click(function(e) {
+    if (!$(this).is('.active')) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  });
+
+  function setEncoded(link, name, data) {
+    var encodedData = encodeURIComponent(data);
+
+    if (data) {
+      link.addClass('active').attr({
+        'href': 'data:application/bpmn20-xml;charset=UTF-8,' + encodedData,
+        'download': name
+      });
+    } else {
+      link.removeClass('active');
+    }
+  }
+
+  var exportArtifacts = debounce(function() {
+
+    saveSVG(function(err, svg) {
+      setEncoded(downloadSvgLink, 'board.svg', err ? null : svg);
+    });
+
+    saveDiagram(function(err, xml) {
+      setEncoded(downloadLink, 'board.postit', err ? null : xml);
+    });
+  }, 500);
+
+  modeler.on('commandStack.changed', exportArtifacts);
+});
+
 
 openDiagram(diagramXML);
 
-// wire save button
-$('#save-button').click(exportDiagram);
+
+// helpers //////////////////////
+
+function debounce(fn, timeout) {
+  var timer;
+
+  return function() {
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    timer = setTimeout(fn, timeout);
+  };
+}
