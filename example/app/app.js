8662847a571d4424555fb0cacabb2ade48e0a775
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-04-17 10:00:23 +0200

    chore(example): add open files functionality

--- example/app/app.js
+++ example/app/app.js
@@ -16,6 +16,44 @@ var modeler = new PostItModeler({
   },
 });
 
+function openFile(file, callback) {
+
+  console.log(file);
+
+  // check file api availability
+  if (!window.FileReader) {
+    return window.alert(
+      'Looks like you use an older browser that does not support drag and drop. ' +
+      'Try using a modern browser such as Chrome, Firefox or Internet Explorer > 10.');
+  }
+
+  // no file chosen
+  if (!file) {
+    return;
+  }
+
+  var reader = new FileReader();
+
+  reader.onload = function(e) {
+
+    var xml = e.target.result;
+
+    callback(xml);
+  };
+
+  reader.readAsText(file);
+}
+
+var fileInput = $('<input type="file" />').appendTo(document.body).css({
+  width: 1,
+  height: 1,
+  display: 'none',
+  overflow: 'hidden'
+}).on('change', function(e) {
+  openFile(e.target.files[0], openDiagram);
+});
+
+
 function openDiagram(bpmnXML) {
   // import diagram
   modeler.importXML(bpmnXML, function(err) {
@@ -41,6 +79,9 @@ $(function() {
 
   var downloadLink = $('#js-download-diagram');
   var downloadSvgLink = $('#js-download-svg');
+
+  var openNew = $('#js-open-new');
+  var openBoard = $('#js-open-diagram');
 
   $('.buttons a').click(function(e) {
     if (!$(this).is('.active')) {
@@ -69,11 +110,24 @@ $(function() {
     });
 
     saveDiagram(function(err, xml) {
-      setEncoded(downloadLink, 'board.postit', err ? null : xml);
+      setEncoded(downloadLink, 'board.xml', err ? null : xml);
     });
   }, 500);
 
   modeler.on('commandStack.changed', exportArtifacts);
+
+  openNew.click(function() {
+    openDiagram(diagramXML);
+  });
+
+  openBoard.on('click', function() {
+    var input = $(fileInput);
+
+    // clear input so that previously selected file can be reopened
+    input.val('');
+    input.trigger('click');
+  });
+
 });
 
 
