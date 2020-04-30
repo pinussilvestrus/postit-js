5cb75ec2912283fbe07f66cd814336c0802832ac
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-04-30 14:46:44 +0200

    chore(example): fix linting error

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,6 +1,6 @@
-const webpack = require("webpack");
+const webpack = require('webpack');
 
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 
 const SENTRY_DSN = process.env.SENTRY_DSN;
@@ -8,38 +8,38 @@ const SOURCE_VERSION = process.env.SOURCE_VERSION || process.env.npm_package_git
 
 module.exports = {
   entry: {
-    bundle: ["./app/app.js"],
+    bundle: ['./app/app.js'],
   },
   output: {
-    path: __dirname + "/public",
-    filename: "app.js",
+    path: __dirname + '/public',
+    filename: 'app.js',
   },
   module: {
     rules: [
       {
         test: /\.xml$/,
-        use: "raw-loader",
+        use: 'raw-loader',
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
     ],
   },
   plugins: [
-    new CopyWebpackPlugin([{ from: "**/*.{html,css}", context: "app/" }]),
+    new CopyWebpackPlugin([{ from: '**/*.{html,css}', context: 'app/' }]),
     new webpack.DefinePlugin({
       'process.env.SENTRY_DSN': JSON.stringify(SENTRY_DSN || null),
       'process.env.SOURCE_VERSION': JSON.stringify(SOURCE_VERSION || null)
     }),
     ...sentryIntegration()
   ],
-  mode: "development",
-  devtool: "source-map",
+  mode: 'development',
+  devtool: 'source-map',
 };
 
 function sentryIntegration() {
